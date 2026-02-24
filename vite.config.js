25dd87b7a9f8d4b4c51cfcc314915f0fb195230a
Chen chen <43808882+bitifyChen@users.noreply.github.com>
2026-02-24 14:48:05 +0800

    Fix:setting

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Layouts from 'vite-plugin-vue-meta-layouts';
