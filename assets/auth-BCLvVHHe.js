8107721c8c5760c1d95c546d383cd459cf7abb8a
bitifyChen <bitifyChen@users.noreply.github.com>
2026-07-30 08:35:30 +0000

    deploy: a4c8826f245126d2ef597da00b87528178a2e102

--- assets/auth-BCLvVHHe.js
+++
@@ -1,1 +0,0 @@
-import{dg as e,dh as a,da as n,a2 as c}from"./index-OaoC2pvy.js";const i=n(c),l=new a,p=()=>new Promise(async(r,t)=>{try{const s=(await e(i,l)).user;console.log("✅ 登入成功:",s.displayName),r({status:200,user:s})}catch(o){console.error("❌ 登入失敗:",o),t(o)}});export{p as l};
